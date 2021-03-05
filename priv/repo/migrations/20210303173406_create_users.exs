fde9747152a0c801548cf737ffa14bd20a03c093
jacrdn <77866322+jacrdn@users.noreply.github.com>
2021-03-05 13:12:11 -0500

    Update 20210303173406_create_users.exs

--- priv/repo/migrations/20210303173406_create_users.exs
+++ priv/repo/migrations/20210303173406_create_users.exs
@@ -4,7 +4,7 @@ defmodule Events.Repo.Migrations.CreateUsers do
   def change do
     create table(:users) do
       add :name, :string, null: false
-      add :email, :string, null: false
+      add :email, :string, null: false, primary_key: true
 
       timestamps()
     end
